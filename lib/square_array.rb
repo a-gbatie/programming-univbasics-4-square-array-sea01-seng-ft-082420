7c4399725605a61bd1383359ce917ae3709a43c2
Ange Batie <ange.batie@gmail.com>
2020-08-10 05:24:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   count = 0 
   while count < array.length do
-    array[count] ** 2
+    new_array << array[count] ** 2
     count += 1 
   end 
   puts new_array
